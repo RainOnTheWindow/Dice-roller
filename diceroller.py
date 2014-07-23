b7f16064245b074d23b2621f5f5997c05a1bc228
Riley Seaman <riley.seaman@gmail.com>
2014-07-23 15:51:45 -0500

    Switched rerolling to use while loop, minor cosmetic changes

--- diceroller.py
+++ diceroller.py
@@ -1,33 +1,31 @@
 import random
 
+list_rolls = list()
+reroll     = True
 
 def roll(num):
     return random.randint(1, int(num))
 
 
-list_rolls = list()
-
-
 def begin():
-    num = raw_input('How many sides would you like your dice to have?')
-    answer = raw_input('How many dice would you like to roll?')
+    num = raw_input('How many sides would you like your dice to have? ')
+    answer = raw_input('How many dice would you like to roll? ')
     for i in range(int(answer)):
         list_rolls.append(roll(num))
 
     print("Here are your results: " + str(list_rolls))
     print("The total value is " + str(sum(list_rolls)))
-    reroll()
 
 
-def reroll():
-    reroll = raw_input('Would you like to roll again? y or n?')
-    if reroll == "y":
-        del list_rolls[:]
-        begin()
+while reroll is True:
+    print("Welcome to the dice roller.")
+    begin()
+
+    ask_reroll = raw_input('Would you like to roll again? y or n?')
+    if ask_reroll.lower() == "y":
+        list_rolls = list()
     else:
         print('Thank-you for using the dice roller!')
-        exit
 
-
-print("Welcome to the dice roller.")
-begin()
+        reroll = False
+        
